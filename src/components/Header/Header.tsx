ae60decff5e3f0c5c9fbf8cb30d277ae95782d6e
Jorge Monroy <jlmonroy13@gmail.com>
2024-07-03 23:09:10 -0500

    Add header menu on mobile

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,9 +2,25 @@
 import Image from "next/image";
 import Link from "next/link";
 import Button from "@/components/Button";
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiX } from "react-icons/fi";
+import { useState, useEffect } from "react";
 
 function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const handleMobileMenuToggle = () => {
+    console.log('isMobileMenuOpen', isMobileMenuOpen)
+    setIsMobileMenuOpen((prev) => !prev);
+  };
+
+  useEffect(() => {
+    if (isMobileMenuOpen) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+  }, [isMobileMenuOpen]);
+
   return (
     <header className="py-4 px-5 lg:px-0 max-w-wrapper mx-auto flex justify-between items-center absolute left-0 top-0 right-0">
       <Link href="/" className="flex items-center gap-3">
@@ -32,10 +48,57 @@ function Header() {
       </div>
       <button
         className="md:hidden text-gray-900 dark:text-white focus:outline-none"
-        onClick={() => {}}
+        onClick={handleMobileMenuToggle}
       >
-        <FiMenu className="h-6 w-6" />
+        {isMobileMenuOpen ? (
+          <FiX className="h-6 w-6" />
+        ) : (
+          <FiMenu className="h-6 w-6" />
+        )}
       </button>
+
+      <div
+        className={`fixed top-0 right-0 h-full w-full bg-white dark:bg-gray-800 shadow-md p-5 transform transition-transform duration-300 ease-in-out ${
+          isMobileMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <Link href="/" className="flex items-center gap-3">
+          <Image width={37} height={40} src="/logo.svg" alt="EarthFast logo" />
+          <h1 className="font-bold text-lg sm:text-2xl">EarthFast</h1>
+        </Link>
+        <button
+          className="absolute top-5 right-5 text-gray-900 dark:text-white focus:outline-none"
+          onClick={handleMobileMenuToggle}
+        >
+          <FiX className="h-6 w-6" />
+        </button>
+        <nav className="flex flex-col items-center space-y-4 mt-16">
+          <Link
+            href="/page1"
+            className="text-title-light dark:text-title-dark text-lg"
+          >
+            Solution
+          </Link>
+          <Link
+            href="/page2"
+            className="text-title-light dark:text-title-dark text-lg"
+          >
+            Resource
+          </Link>
+          <Link
+            href="/page3"
+            className="text-title-light dark:text-title-dark text-lg"
+          >
+            Company
+          </Link>
+          <Button href="/" variant="text">
+            Login
+          </Button>
+          <Button href="/" variant="inverted" className="leading-3 !py-3">
+            Get Started
+          </Button>
+        </nav>
+      </div>
     </header>
   );
 }
