1eb271734c970cd8573084340a3bdc2079856233
Jorge Monroy <jlmonroy13@gmail.com>
2024-07-03 22:34:20 -0500

    Slider functionality

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,16 +1,33 @@
 "use client";
 
+import { useRef } from "react";
 import Image from "next/image";
+import SlickSlider from "react-slick";
 import cx from "classnames";
 import { Roboto_Mono } from "next/font/google";
 import { TESTIMONIALS } from "@/data/staticInfo";
 import { LiaChevronCircleLeftSolid, LiaChevronCircleRightSolid } from "react-icons/lia";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./slider.css";
+
 const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
+const settings = {
+  dots: false,
+  arrows: false,
+  infinite: true,
+  speed: 500,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+};
+
 const Slider = () => {
+  const sliderRef = useRef<SlickSlider>(null);
+
   return (
-    <div className="bg-gradient-to-b dark:from-[#2E93FF] dark:to-[#007DD8] from-[#2E93FF]/[0.3] to-[#2E93FF] rounded-lg max-w-[87rem] sm:mx-auto px-5 pt-8 pb-10 sm:pt-16 sm:px-12 sm:pb-14 relative mx-3">
+    <div className="slider bg-gradient-to-b dark:from-[#2E93FF] dark:to-[#007DD8] from-[#2E93FF]/[0.3] to-[#2E93FF] rounded-lg max-w-[87rem] sm:mx-auto px-5 pt-8 pb-10 sm:pt-16 sm:px-12 sm:pb-14 relative mx-3">
       <div className="flex flex-col md:flex-row items-start justify-between">
         <div className="mb-10 sm:mb-0">
           <h2
@@ -30,37 +47,39 @@ const Slider = () => {
             stagnant, high-cost market.
           </p>
         </div>
-        <div className="md:mt-0 bg-white dark:bg-white/[0.20] rounded-3xl transition-transform duration-500 ease-in-out transform max-w-full sm:max-w-[39.75rem] p-6 sm:py-12 sm:px-10 min-h-[21.75rem]">
-          <div className="flex items-center mb-6 sm:mb-10">
-            <Image
-              src={TESTIMONIALS[0].logo}
-              alt={`${TESTIMONIALS[0].company} Logo`}
-              className="mr-4 dark:hidden"
-              width={170}
-              height={32}
-            />
-            <Image
-              src={TESTIMONIALS[0].whiteLogo}
-              alt={`${TESTIMONIALS[0].company} Logo`}
-              className="mr-4 dark:block hidden"
-              width={170}
-              height={32}
-            />
-          </div>
-          <p className="text-body-light dark:text-white text-base sm:text-2xl">
-            {TESTIMONIALS[0].quote}
-          </p>
-        </div>
+        <SlickSlider ref={sliderRef} {...settings}>
+          {TESTIMONIALS.map(({ logo, whiteLogo, company, quote }, i) => (
+            <div key={`${company}-${i}`} className="px-2">
+              <div className="md:mt-0 bg-white dark:bg-white/[0.20] rounded-3xl max-w-full sm:max-w-[39.75rem] p-6 sm:py-12 sm:px-10 min-h-[21.75rem]">
+                <div className="flex items-center mb-6 sm:mb-10">
+                  <Image
+                    src={logo}
+                    alt={`${company} Logo`}
+                    className="mr-4 !dark:hidden"
+                    width={170}
+                    height={32}
+                  />
+                  <Image
+                    src={whiteLogo}
+                    alt={`${company} Logo`}
+                    className="mr-4 !dark:block !hidden"
+                    width={170}
+                    height={32}
+                  />
+                </div>
+                <p className="text-body-light dark:text-white text-base sm:text-2xl">
+                  {quote}
+                </p>
+              </div>
+            </div>
+          ))}
+        </SlickSlider>
       </div>
       <div className="flex justify-center md:justify-start sm:absolute sm:bottom-8 sm:left-6 gap-2 mt-6 sm:mt-0">
-        <button
-        // onClick={handlePrev}
-        >
+        <button onClick={() => sliderRef?.current?.slickPrev()}>
           <LiaChevronCircleLeftSolid size={44} color="black" />
         </button>
-        <button
-        // onClick={handleNext}
-        >
+        <button onClick={() => sliderRef?.current?.slickNext()}>
           <LiaChevronCircleRightSolid size={44} color="black" />
         </button>
       </div>
