a0378da8eb01a08a35073b4cc0616adb0a7aca97
Jorge Monroy <jlmonroy13@gmail.com>
2024-08-06 14:17:13 -0500

    In dark mode, switch the favicon to the dark mode one

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,22 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Earth Fast - Decentralize your frontend, securely.",
-  description: "EarthFast is an infrastructure protocol that shifts the responsibility of updating, deploying and hosting your frontend to a community of voters.",
+  description:
+    "EarthFast is an infrastructure protocol that shifts the responsibility of updating, deploying and hosting your frontend to a community of voters.",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/icon.ico",
+        href: "/icon.ico",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/icon-dark.ico",
+        href: "/icon-dark.ico",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
